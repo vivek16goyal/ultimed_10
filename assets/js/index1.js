94775d9304daeca2b84173ea877d3611f27723f2
vivek16goyal <vivek.umredkar@goyalonline.com>
2017-05-13 11:02:24 +0530

    change in images

--- assets/js/index1.js
+++ assets/js/index1.js
@@ -60,8 +60,8 @@ var pDocReq = "1";
 
 var SMSFrom = "md-goyals";
 var loadmsg="Please Wait....";
-var GBCServicePath = "http://tiaapp.goyalonline.in/";
-//var GBCServicePath = "http://localhost:51738/";
+//var GBCServicePath = "http://tiaapp.goyalonline.in/";
+var GBCServicePath = "http://localhost:51738/";
 var pictureSource;
 var destinationType;
 var folderPath = "file:///storage/sdcard0";
@@ -94,7 +94,7 @@ function onDeviceReady() {
     discount();
    
     // var charge = $("#charges").text().toString().replace("Rs.", "");
-    //  $("#td3").text().toString().fixed
+    //  $("#td3").text().toString().fixed;
     pDiscPer = localStorage.getItem("label");
 
     $("#td3").text(Number(pDiscPer).toFixed(2) + "% Dis");
@@ -102,12 +102,12 @@ function onDeviceReady() {
         $("#div_statecity").hide();
     }
     else {
-        slider();
+       // slider();
         $("#div_statecity").show();
         try {
             if (HomeSlideCnt == "1") {
                 $("#HomeSlider-div").html("<ul  id='sliderFrame' style='display: none' > <li id='slide-1'>" +
-                                         "<img src='assets/img/Slide-11.jpg' />" +
+                                         "<img src='assets/img/Slide-11.jpg' />" +"<img src='assets/img/Slide-11.jpg' />"+
                                          "</li></ul>");
                 $("#sliderFrame").addClass("slides");
                 $("#sliderFrame li").show();
@@ -186,7 +186,6 @@ function onDeviceReady() {
     catch (e) {
         alert(e.message);
     }
-
 }
 //change link item cart
 function cliencode_check() {
@@ -373,58 +372,66 @@ function validateEmail(emailField) {
 //}
 //ultimate change for the image
 function getimage() {
-   // var startdt;
-     //   var enddt;
-        WebSerUrl = localStorage.getItem("APIURL");
-        $.ajax({
-            url: WebSerUrl + "/APPSETTING/GETIMAGE",
-            type: "GET",
-            cache: false,
-            success: function (data, id) {
-              //  debugger;
-                {                                     
-                    //localStorage.setItem("IMAGE01",data.[0]);
-                    var img01 = data[0].IMG2;
-                    CONVERSION_IMAGE(img01);
-                    // Convert the string to bytes
-                    
-                    var img02 = data[0].IMG2;
-                    var img03 = data[0].IMG3;
-                    var img04 = data[0].IMG4;
-                    var img05 = data[0].IMG5;
-                    }
-                
-                },
-            //if any error occure
-            error: function (xmlHttpRequest, textStatus, errorThrown) {
-                console.log('some error occured', textStatus, errorThrown);
-            }
- 
-        });
+//image 1
+    var img = document.createElement("img");
+    var api = localStorage.getItem("APIURL");
+   img.src = api+"/VrImg/img_app/002.jpg";
+   var src = document.getElementById("img01");
+   img.id = "id1";
+   src.appendChild(img);
+   document.getElementById("id1").style.width = "100%";
+   document.getElementById("id1").style.height = "100%";
+    //image 2
+   var img = document.createElement("img");
+   img.src = api+"/VrImg/img_app/003.jpg";
+   var src = document.getElementById("img02");
+   img.id = "id2";
+   src.appendChild(img);
+   document.getElementById("id2").style.width = "100%";
+   document.getElementById("id2").style.height = "100%";
+    //image 3
+   var img = document.createElement("img");
+   img.src =api+ "/VrImg/img_app/004.jpg";
+   var src = document.getElementById("img03");
+   img.id = "id3";
+   src.appendChild(img);
+   document.getElementById("id3").style.width = "100%";
+   document.getElementById("id3").style.height = "100%";
+    //image 4
+   var img = document.createElement("img");
+   img.src =api+ "/VrImg/img_app/005.jpg";
+   var src = document.getElementById("img04");
+   img.id = "id4";
+   src.appendChild(img);
+   document.getElementById("id4").style.width = "100%";
+   document.getElementById("id4").style.height = "100%";
+    //image 5
+   var img = document.createElement("img");
+   img.src = api+"/VrImg/img_app/006.jpg";
+   var src = document.getElementById("img05");
+   img.id = "id5";
+   src.appendChild(img);
+   document.getElementById("id5").style.width = "100%";
+   document.getElementById("id5").style.height = "100%";
+    //
+    //cleared chache
+   //var target = $('.cached-img');
 
+   //ImgCache.isCached(target.attr('src'), function (path, success) {
+   //    if (success) {
+   //        // already cached
+   //        ImgCache.useCachedFile(target);
+   //    } else {
+   //        // not there, need to cache the image
+   //        ImgCache.cacheFile(target.attr('src'), function () {
+   //            ImgCache.useCachedFile(target);
+   //        });
+   //    }
+    //});
+   flushdata(obj)
+   //flushdata(obj, mode)
   }
-function CONVERSION_IMAGE()
-{
-    var img01 = '424D5E070000000000003E00000028000000EF000000390000000100010000000000200700000000000000000000020000000000000000000000FFFFFF00FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE0000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE0000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE0000E00003FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE0000E000007FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE0000E3FFF80FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE0000E7FFFFC3FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE0000E7FFFFF8FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE0000E7FFFFFE7FFFFFFFFFFFFFFFFFFFFFFFF001FFFFFFFFFFFFFFFFFFFFFFFE0000E7FFFFFF7FFFFFFFFFFFFFFFFFF83FFFC0FE7FFFFFFFFFFFFFFFFFFFFFFE0000E7FFFFFFBFFFFFFFFFFFFFFFFFF01FFF07FF9FFFFFFFFFFFFFFFFFFFFFFE0000C7FFFFFFDFFFFFFFFFFFFFFFFFE0EFFE0FFFE3FFFFFFFFFFFC0FFE007FFE0000C7FFFFFFEFFFFFFFFFFFFFFFFFE3EFFC3FFFFBFFFFFFFFFFE0F7F80FBFFE0000C7FFFFFFEFFFFFFC1FFFFFFFFFE3EFFC7FFFFDFFFFFFFFFFC7FBF03FBFFE0000C7FFFFFFEFFFFFF007FFFFFFE0E3EFFC7FFFFDFFFE01FFFF8FFBF0FFBFFE0000C7FFFFFFEFFFFFE0F7FFFFFF80E3EFFC7FFFFEFFFE307FFF1FFBE3FFBFFE0000C7FFFFFFEFFFFFE3FBFFFFFF0763EFFE7FFFFEFC3E7F77FF1FFBC7FFBFFE0000C7FFFFFFEFFFFFE3FDFFFFFE1F63F7FE7F07FF7800FF70FE3FFDC7FFBFFE0000C7FFFFFFEFFFFFF3FDFFFFFC7F63F7FF3EF9FF78E0FF633E3FFD8FFF7FFE0000C7FFFFFFEFFFFFF3FEFFFFFCFF67F7FF9DFCFF71FFFF63BC3FFD9FFCFFFE0000CFFFFFFFEFFFFFF9FEFFC0F8FF47FBFFC3FCFFB1FFFF63CC7FFB1FF3FFFE0000CFE000FFDFFC03F8FF7E0030FF07F9FFFFFC7FB1FFFF67F47FE73FCFFFFE0000CFDFFEFFDFF830FEFFBC1FB1FC8FFEFFFFFE3FB3FFFF47F4FFDE7FBFFFFE0000CF9FFEFFBFF0FE7E7FBC3FB1FB8FFF7FFFFE3FD3FFFF47F4FFBE7F9FFFFE0000CF1FFC7F7FE1FF3F3FDC7FD3F78FFFBFFFFE3FD3FFFF47F0FF3E7F8FFFFE00008F9FE0FF7FE3FFBF3FDC7FD3F7CFFFDFFFFE3FD3FFFF4FF8FF7E7FC7FFFE00008F8000FEFFE3FFDF9FDC7FD3F3E43FEFFFFE3FD3FFFF4FF8FF7CFFE3FFFE00008FC003FC3FC7FFDF9FDC7FB3F87BBFEFFFFE3FD3FFFF4FF8FF7CFFF3FFFE00008FFEFFF007C7FFEF9FDC7F73FF3FCFEFFFFE3FD3F1FF0FF0FFBCFFF9FFFE00008FFFFFC001C7C3EF1FDC7EFBFF9FF7EFFFFE3FF3F6FF0FF4FFBCFFF9FFFE00008FFFFE21FCC7D9EE1FDC7DF9FFEFFBEFFFFE3FE3F6FF0FF4FFDCFCFCFFFE00008FFFFEC3FF07D8E43FBC7DF9E0F7FC0FFFFE3FD3F67F0FF47FE4FB3EFFFE00008FFFFF47FF87D8F07F7C7DFCEE77FFFFFFFE3FD3F73F0FE47FF2FB9EFFFE00008FFFFF47FFC7D8F0FF007C02EE77FFFFFFFE3FD3F73F0FEE7FFC7B1DFFFE00009FFFFF8FFFC7D0F0FE07FE02F477FFFFFFFC3FD3F73F4C4E7FFD7C3DFFFE00009FFFFF8FFFE7D1E1FE8FFFFCF877FFFFFFFC7FD3F73F41BE3FFD7C3BFFFE00009FFFFF8FC3E3E1E1FD8FFFFE78F7FFFFFFFC7FD3F73F6CFE3FFDBFFBFFFE00009FFFFF8F9BE3F1E3FDCFFFFEBFF7FFFFFFFC7FD3FB3F787F3FFDBFFBFFFE00009E07FF8FB9F3FFE3FDCFFFFEDFEFFFFFFFFC7FD9FB3F783F1FFDDFF7FFFE00009EF9FF8F79F1FFE3FDE7FFFEE79FFFFFFFF87FD9FBBF71DF9FFDEFCFFFFE00009EFDFF0F39F5FFE3FEE7FFFDF87FFFFFFFF8FFDDFB9F73DF9FFDF03FFFFE00001EFDFF4FB9E6FFC9FF381F03FFFFFFFFFFF8FF9DFB9FA3EFCFFBFFFFFFFE00001EF0FEEF91EF7FBCFFCF9F7FFFFFFFFFFFF0FFBE03DFA7EFCFFBFFFFFFFE00001E01FEE7D1EFBF7E7FF78FBFFFFFFFFFFFF1FFBFFFCFA7EFC7FBFFFFFFFE00001E01FEE7C1CFDE7F7FFB8FBFFFFFFFFFFFF1FFBFFFE7A7EFE3F7FFFFFFFE00001FFFFEF7E3CFE1FFBFFB8FBFFFFFFFFFFFF1FFBFFFF027EFF1F7FFFFFFFE00001FFFFCF3FFDFFFFFDFFB8FBFFFFFFFFFFFF3FF7FFFFFF7EFFCEFFFFFFFFE00009FFFF9FBFFDFFFFFEFFBCFBFFFFFFFFFFFF3FF7FFFFFF3EFFF0FFFFFFFFE0000C7FFE7FDFFDFFFFFF7F7CFBFFFFFFFFFFFE3FF7FFFFFF3EFFFFFFFFFFFFE0000F03F9FFEFFDFFFFFF7F7C7BFFFFFFFFFFFE7FF7FFFFFFBDFFFFFFFFFFFFE0000FF807FFEFFBFFFFFF9EFE7BFFFFFFFFFFFE7FF7FFFFFFDBFFFFFFFFFFFFE0000FFFDFFFF7F3FFFFFFE1FF07FFFFFFFFFFFC7FF7FFFFFFE3FFFFFFFFFFFFE0000FFFFFFFF40FFFFFFFFFFFFFFFFFFFFFFFFE7FEFFFFFFFFFFFFFFFFFFFFFE0000FFFFFFFFBFFFFFFFFFFFFFFFFFFFFFFFFFF3F1FFFFFFFFFFFFFFFFFFFFFE0000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC0FFFFFFFFFFFFFFFFFFFFFFE0000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE0000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE0000';
-    var bytes = new Uint8Array(img01.length / 2);
 
-    for (var i = 0; i < img01.length; i += 2) {
-        bytes[i / 2] = parseInt(img01.substring(i, i + 2), /* base = */ 16);
-    }
-
-    // Make a Blob from the bytes
-    var blob = new Blob([bytes], { type: 'image/bmp' });
-
-    // Use createObjectURL to make a URL for the blob
-    var image = new Image();
-    image.src = URL.createObjectURL(blob);
-
-    //var fso = new ActiveXObject("Scripting.FileSystemObject");
-    //var fh = fso.CreateTextFile("c:\\+image+", true);
-    //fh.WriteLine("Some text goes here...");
-    //fh.Close();
-    $("#Image1").append(image);
-}
 function SendLedgerEmail() {
     var pcode = "A10000";
     var frmdate = $("#frmdt_ldg1").val();//CHANGE BY VIVEK TEMPIRARY
