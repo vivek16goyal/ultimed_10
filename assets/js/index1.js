cd27f3ac44c96f362fe706d091f88c9c4db6f365
vivek16goyal <vivek.umredkar@goyalonline.com>
2017-05-24 13:14:16 +0530

    as

--- assets/js/index1.js
+++ assets/js/index1.js
@@ -36,7 +36,7 @@ var APPType = "$";
 
 //  3)///////*******A to Z Dava [Demo Version Given To Drug Deal] **********************//////////
 var Heading = "ULTIMED";
-var PCODE = "dem001 ";
+var PCODE = "DEM010  ";
 var folderPathImg = "file:///storage/sdcard0/" + PCODE + "/";
 var DownloadPath = "file:///storage/sdcard0/";
 var FolderName = PCODE;
@@ -3418,7 +3418,7 @@ function SaveOrder() {
                            // var i ;
                             // parseInt( i );
 
-                          if (smallImage.src.indexOf("No_image.png") < 0)
+                       //   if (smallImage.src.indexOf("No_image.png") < 0)
                             {
                             
                             {
